e4f053ed0d7eaab80a2cb08510dc44695db08dba
dennisobare <dennisobare338@gmail.com>
2024-06-20 18:37:43 +0300

    Created a landing page for a portifolio website

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+console.log("I'm learning Javascript");
+// This is a comment.
+let message = "Hello World!";
+console.log(message);
+let person = "John";
+console.log(`${person} says ${message}`);
